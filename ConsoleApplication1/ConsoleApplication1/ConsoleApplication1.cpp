46f3143d876a705789c8bc7944025b61eff02926
gary50360214 <gary50360214@gmail.com>
2019-02-27 20:58:02 +0800

    test03

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,21 +1,21 @@
 #include "stdafx.h"
+#include "stdafx.h"
 #include <string>
 #include <iostream>
 using namespace System;
-bool Guess(int number) {
-static int target = -1;
-if (target == -1) {
-Random r;
-target = r.Next() % 100 + 1;
-}
-if (number == target)return true;
-return false;
-}
-int main(array<System::String ^> ^args)
+bool Guess(int number)
 {
-int guess;
-do {
-std::cin >> guess;
-} while (!Guess(guess));
-return 0;
+}
+int main(array<System::String^>^ args)
+{
+    int guess;
+
+    do
+    {
+        std::cout << "Choose a number between 1 - 100 :";
+        std::cin >> guess;
+    }
+    while (!Guess(guess));
+
+    return 0;
 }
